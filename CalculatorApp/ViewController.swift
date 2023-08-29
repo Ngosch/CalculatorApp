eb94c03e15d1211dcd61f0095fcb1fdffd489253
Ngosch <carl1312@gmail.com>
2023-08-29 13:55:47 +0900

    連続計算

--- CalculatorApp/ViewController.swift
+++ CalculatorApp/ViewController.swift
@@ -1,4 +1,3 @@
-////
 //  ViewController.swift
 //  CalculatorApp
 //
@@ -17,18 +16,61 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var textLabel: UILabel!
     
+    // グラデーションレイヤーをプロパティとして保持
+    let gradientLayer = CAGradientLayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // グラデーションの初期設定
+        gradientLayer.frame = self.view.bounds
+        updateGradientColors()
+        self.view.layer.insertSublayer(gradientLayer, at: 0)
+        
         textLabel.text = ""
     }
     
+    // グラデーションの色をランダムに更新する関数
+    func updateGradientColors() {
+        let colors = [randomColor(), randomColor()]
+        gradientLayer.colors = colors.map { $0.cgColor }
+    }
+    
+    // ランダムな色を生成する関数
+    func randomColor() -> UIColor {
+        return UIColor(
+            red: CGFloat(drand48()),
+            green: CGFloat(drand48()),
+            blue: CGFloat(drand48()),
+            alpha: 1.0
+        )
+    }
+    
     @IBAction func pressNum(_ sender: UIButton) {
+        // アニメーションの追加
+        sender.transform = CGAffineTransform(scaleX: 0.6, y: 0.6)
+        UIView.animate(withDuration: 0.2,
+                       delay: 0,
+                       usingSpringWithDamping: 0.2,
+                       initialSpringVelocity: 6.0,
+                       options: .allowUserInteraction,
+                       animations: { [weak self] in
+                        sender.transform = .identity
+                       },
+                       completion: nil)
+        
         var pushed_num = (sender.titleLabel?.text)!
         print(pushed_num + "が入力されました。")
+        if afterCaliuculationFlag {
+            firstNumStr = resultNumStr
+            secondNumStr = ""
+            afterCaliuculationFlag = false
+        }
+        
         if enteredFirstNumFlag == false {
             firstNumStr += pushed_num
             textLabel.text = firstNumStr
-        } else if enteredFirstNumFlag && afterCaliuculationFlag == false {
+        } else {
             secondNumStr += pushed_num
             textLabel.text = secondNumStr
         }
@@ -37,41 +79,55 @@ class ViewController: UIViewController {
     @IBAction func pressOperator(_ sender: UIButton){
         var pushed_arithmetic = (sender.titleLabel?.text)!
         print(pushed_arithmetic + "が入力されました。")
+        if afterCaliuculationFlag {
+            firstNumStr = resultNumStr
+            secondNumStr = ""
+            afterCaliuculationFlag = false
+        }
+        
         if enteredFirstNumFlag == false && firstNumStr != "" {
-            operatorStr = (sender.titleLabel?.text)!
+            operatorStr = pushed_arithmetic
             enteredFirstNumFlag = true
+        } else if enteredFirstNumFlag && firstNumStr != "" {
+            operatorStr = pushed_arithmetic
         }
     }
     
     @IBAction func pressEqual(_ sender: UIButton) {
         if firstNumStr != "" && secondNumStr != "" && operatorStr != "" {
             if let firstNumDouble = Double(firstNumStr), let secondNumDouble = Double(secondNumStr) {
+                var resultNum: Double = 0.0
                 switch operatorStr {
                 case "+":
-                    let resultNum = firstNumDouble + secondNumDouble
-                    resultNumStr = String(resultNum)
-                    textLabel.text = resultNumStr
+                    resultNum = firstNumDouble + secondNumDouble
                 case "-":
-                    let resultNum = firstNumDouble - secondNumDouble
-                    resultNumStr = String(resultNum)
-                    textLabel.text = resultNumStr
+                    resultNum = firstNumDouble - secondNumDouble
                 case "*":
-                    let resultNum = firstNumDouble * secondNumDouble
-                    resultNumStr = String(resultNum)
-                    textLabel.text = resultNumStr
+                    resultNum = firstNumDouble * secondNumDouble
                 case "/":
                     if secondNumDouble != 0 {
-                        let resultNum = firstNumDouble / secondNumDouble
-                        resultNumStr = String(resultNum)
-                        textLabel.text = resultNumStr
+                        resultNum = firstNumDouble / secondNumDouble
                     } else {
                         textLabel.text = "エラー！"
                         print("0で割ったため")
+                        return
                     }
                 default: break
                 }
+                // 小数点以下が0の場合は、整数として表示
+                if floor(resultNum) == resultNum {
+                    resultNumStr = String(format: "%.0f", resultNum)
+                } else {
+                    resultNumStr = String(resultNum)
+                }
+                textLabel.text = resultNumStr
                 afterCaliuculationFlag = true
+                enteredFirstNumFlag = false
+                operatorStr = ""
                 print(resultNumStr + "が出力されました。")
+                
+                // 計算結果を表示した後、グラデーションの色を更新
+                updateGradientColors()
             }
         }
     }
@@ -80,6 +136,7 @@ class ViewController: UIViewController {
         firstNumStr = ""
         secondNumStr = ""
         operatorStr = ""
+        resultNumStr = ""
         textLabel.text = ""
         enteredFirstNumFlag = false
         afterCaliuculationFlag = false
